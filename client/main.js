c0bad528f5fd1a465da78a7772df711b3e96c5af
torrent benoit <torrentdu64@gmail.com>
2017-10-02 11:51:58 +0200

    add router

--- client/main.js
+++ client/main.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import App from './components/app';
-import {Bins} from '../imports/collections/bins';
+import { Bins } from '../imports/collections/bins';
 
+const routes = (
+  <BrowserRouter>
+    <div>
+        <Route exact path="/" component={App} />
+    </div>
+  </BrowserRouter>
+);
 
-Meteor.startup( () => {
-  ReactDOM.render( <App />, document.querySelector('.render-target'));
+Meteor.startup(() => {
+  ReactDOM.render(routes, document.querySelector('.render-target'));
 });
