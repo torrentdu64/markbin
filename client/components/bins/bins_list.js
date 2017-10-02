40d98f4d75f0fa01e1cb5ae691aa2e7fb7b19a7c
torrent benoit <torrentdu64@gmail.com>
2017-10-02 11:03:12 +0200

    remove a bin

--- client/components/bins/bins_list.js
+++ client/components/bins/bins_list.js
@@ -6,10 +6,22 @@ import {Bins} from '../../../imports/collections/bins';
 
 class BinsList extends Component {
 
+  onBinRemove(bin) {
+    Meteor.call('bins.remove', bin);
+  }
+
   renderList() {
     return this.props.bins.map( bin => {
         return (
-          <li className="list-group-items" key={bin._id} > bin {bin._id}  </li>
+          <li className="list-group-item" key={bin._id} >
+           bin {bin._id}
+           <span className="pull-right">
+            <button className="btn btn-danger"
+                    onClick={ () => this.onBinRemove(bin)}>
+                    Remove
+            </button>
+           </span>
+          </li>
           );
     });
   }
--- imports/collections/bins.js
+++ imports/collections/bins.js
@@ -9,6 +9,9 @@ Meteor.methods({
         sharedWith: [],
         ownerId: this.userId
     });
+  },
+  'bins.remove': function(bin) {
+    return Bins.remove(bin);
   }
 });
 
