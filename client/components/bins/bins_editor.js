17fcc88a0e89ac57e6bf0844c13e8b7893f54d0f
torrent benoit <torrentdu64@gmail.com>
2017-10-03 17:38:57 +0200

    prb to id i d no

--- client/components/bins/bins_editor.js
+++ client/components/bins/bins_editor.js
@@ -1,12 +1,15 @@
-import React, {Component} from 'react';
-
+import React, { Component } from 'react';
+import CodeMirror from 'react-codemirror';
+import 'codemirror/mode/markdown/markdown';
 
 class BinsEditor extends Component {
   render() {
-    return (
-
-      <div>bins editor</div>
-      );
+    return(
+      <div className="col-xs-8">
+        <h5>Input</h5>
+        <CodeMirror options={{ mode: 'markdown', lineNumbers: true }} />
+      </div>
+    );
   }
 }
 
--- client/components/bins/bins_main.js
+++ client/components/bins/bins_main.js
@@ -1,24 +1,21 @@
-import React , {Component} from 'react';
-import { createContainer} from 'meteor/react-meteor-data';
-import {Bins} from '../../../imports/collections/bins';
+import React, { Component } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
+import { Bins } from '../../../imports/collections/bins';
 import BinsEditor from './bins_editor';
 
-
-
-
 class BinsMain extends Component {
-  render(){
+  render() {
     return (
       <div>
         <BinsEditor bin={this.props.bin} />
       </div>
-      );
+    );
   }
 }
 
+export default createContainer((props) => {
+  const { binId } = props.params;
+  Meteor.subscribe('bins');
 
-export default createContainer( (props) => {
-    const {binId} = props.params;
-    Meteor.subscribe('bins');
-    return { bin: Bins.findOne(binId) };
-},BinsMain);
+  return { bin: Bins.findOne(binId) };
+}, BinsMain);
