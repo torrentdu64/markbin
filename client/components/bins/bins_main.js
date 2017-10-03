d2246f9f554b6a20af591e2aab2a544e0115725a
torrent benoit <torrentdu64@gmail.com>
2017-10-03 16:58:47 +0200

    createContainer to bin_main

--- client/components/bins/bins_main.js
+++ client/components/bins/bins_main.js
@@ -1,4 +1,6 @@
 import React , {Component} from 'react';
+import { createContainer} from 'meteor/react-meteor-data';
+import {Bins} from '../../../imports/collections/bins';
 
 
 
@@ -12,4 +14,8 @@ class BinsMain extends Component {
 }
 
 
-export default BinsMain;
+export default createContainer( (props) => {
+    const {binId} = props.params;
+    Meteor.subscribe('bins');
+    return { bin: Bins.findOne(binId) };
+},BinsMain);
