f0611c29b6dc5838ad434a45b89d4613be472622
torrent benoit <torrentdu64@gmail.com>
2017-10-04 13:47:11 +0200

    markdown view

---
+++ client/components/bins/bins_viewer.js
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+
+
+
+
+class BinsViewer extends Component {
+  render(){
+    return (
+      <div className="col-xs-4">
+        <h4>output</h4>
+      </div>
+
+      );
+  }
+}
+
+export default BinsViewer;
