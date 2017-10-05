60c8c2df144405b6b100e5f6d594473a46c1e914
torrent benoit <torrentdu64@gmail.com>
2017-10-05 12:05:42 +0200

    share bin don

--- server/main.js
+++ server/main.js
@@ -9,5 +9,11 @@ Meteor.startup(() => {
   Meteor.publish('sharedBins', function (){
     const user = Meteor.users.findOne(this.userId);
     if(!user) {return ;}
-  })
+
+    const email = user.emails[0].address ;
+
+    return Bins.find({
+        sharedWith: { $elemMatch: { $eq: email}}
+    });
+  });
 });
