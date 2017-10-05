3746b703db0ed9e25ece4a0d7906b265b0ae9e6e
torrent benoit <torrentdu64@gmail.com>
2017-10-05 10:32:34 +0200

    share bin

---
+++ client/components/bins/bins_share.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+
+
+class BinsShare extends Component {
+  render() {
+    return (
+      <footer className="bin-share" >
+          <div className="input-group">
+            <input className="form-control" />
+            <div className="input-group-btn">
+              <button  class="btn btn-default">
+              Share
+              </button>
+            </div>
+          </div>
+      </footer >
+      );
+  }
+}
+
+
+
+
+export default BinsShare;
